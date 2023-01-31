1f56b12bfc0b18df76e24bfae1a283b18858ea9a
amanfangeria980 <amanfangeria980@gmail.com>
2023-01-31 19:22:48 +0530

    NextGreaterElementToTheRight2.cpp modified

--- Stack/Pepcoding/Stack 1/NextGreaterElementToTheRight2.cpp
+++ Stack/Pepcoding/Stack 1/NextGreaterElementToTheRight2.cpp
@@ -40,3 +40,39 @@ int main(){
 // 1.NextSmallerElementToTheRight
 // 2.NextGreaterElementToTheLeft
 // 3.NextSmallerElementToTheLeft
+
+
+//Best Solution :
+
+//right
+/* 
+vector<int> getNextGreaterElement (vector<int> &arr) {
+	stack<int> st;
+	int n=arr.size();
+	vector<int> ans(n);
+	st.push(n-1);
+    ans[n-1]=-1;   
+	for(int i=n-2;i>=0;i--){
+		while(!st.empty() && arr[st.top()]<arr[i]){
+			st.pop();
+		}
+        if(st.empty()){
+            ans[i]=-1;
+        }
+        else{
+            ans[i]=arr[st.top()];
+        }
+		st.push(i);
+	}
+	return ans;
+}
+
+int main(){
+	vector<int> v={1,2,3,4,2,9,8,5,6};
+	vector<int> ans=getNextGreaterElement(v);
+	for(auto it : ans){
+		cout<<it<<" ";
+	}
+	return 0;
+}
+ */
