4daeac6c53b10cf25fb2f231e8c7e0d2ee3b2a0d
amanfangeria980 <amanfangeria980@gmail.com>
2023-01-31 12:11:05 +0530

    modified the code a little bit

--- Stack/Pepcoding/Stack 1/NextGreaterElementToTheRight2.cpp
+++ Stack/Pepcoding/Stack 1/NextGreaterElementToTheRight2.cpp
@@ -1,10 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 vector<int> getNextGreaterElement (vector<int> &A) {
 	stack<int> st;
 	int n=A.size();
 	vector<int> ans(n);
 	st.push(0);
 	for(int i=1;i<n;i++){
-		while(!st.empty() && A[i]>A[st.top()]){
+		while(!st.empty() && A[st.top()]<A[i]){  //i is aage wala element 0 1 , i is at 1
 			int pos=st.top();
 			ans[pos]=A[i];
 			st.pop();
@@ -18,6 +21,14 @@ vector<int> getNextGreaterElement (vector<int> &A) {
 	}
 	return ans;
 }
+
+int main(){
+	vector<int> v={1,2,3,4,2,9,8,5,6};
+	vector<int> ans=getNextGreaterElement(v);
+	for(auto it : ans){
+		cout<<it<<" ";
+	}
+	return 0;
 }
 //checking from left to right
 // pop ans (at the same time)
