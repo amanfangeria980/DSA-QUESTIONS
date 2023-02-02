4e4290d351fb4c776ecd61f837c1cd3d98c1003e
amanfangeria980 <amanfangeria980@gmail.com>
2023-02-02 19:05:31 +0530

    Added Sliding Window Maximum program

---
+++ Stack/Pepcoding/Stack 1/7_SlidingWindowMaximum.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int *nger(int arr[], int n)
+{
+    int *ans = new int[n];
+    stack<int> st;
+    ans[n - 1] = n;
+    st.push(n - 1);
+    for (int i = n - 2; i >= 0; i--)
+    {
+        while (!st.empty() && arr[i] > arr[st.top()])
+        {
+            st.pop();
+        }
+        if (st.empty())
+            ans[i] = n;
+        else
+            ans[i] = st.top();
+        st.push(i);
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int *nge = nger(arr, n);
+    int k;
+    cin >> k;
+    int j = 0;
+    for (int i = 0; i <= n - k; i++)
+    {
+        if (j < i)
+            j = i;
+        while (nge[j] < i + k)
+        {
+            j = nge[j];
+        }
+        cout << arr[j] << endl;
+    }
+
+    return 0;
+}
