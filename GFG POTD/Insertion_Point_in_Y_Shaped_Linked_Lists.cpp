4c638ae6d1e3d583b62d0964dfbe0620f6fb264c
amanfangeria980 <amanfangeria980@gmail.com>
2023-02-05 16:50:42 +0530

    5 Feb 2023

---
+++ GFG POTD/Insertion_Point_in_Y_Shaped_Linked_Lists.cpp
@@ -0,0 +1,134 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    if (size == 0) return NULL;
+
+    int val;
+    cin >> val;
+
+    Node *head = new Node(val);
+    Node *tail = head;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        cin >> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+
+    return head;
+}
+
+
+// } Driver Code Ends
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+class Solution
+{
+public:
+    int intersectPoint(Node* head1, Node* head2)
+    {
+        int l1=0;
+        int l2=0;
+        auto temp1=head1,temp2=head2;
+        while(temp1){
+            temp1=temp1->next;
+            l1++;
+        }
+        while(temp2){
+            temp2=temp2->next;
+            l2++;
+        }
+        temp1=head1;
+        temp2=head2;
+        if(l1>l2){
+            int cnt=l1-l2;
+            while(cnt--){
+                temp1=temp1->next;
+            }
+        }
+        
+        if(l2>l1){
+            int cnt=l2-l1;
+            while(cnt--){
+                temp2=temp2->next;
+            }
+        }
+        
+        while(temp1->next!=temp2->next){
+            temp1=temp1->next;
+            temp2=temp2->next;
+        }
+        return temp1->next->data;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+
+/* Driver program to test above function*/
+int main()
+{
+    int T, n1, n2, n3;
+
+    cin >> T;
+    while (T--)
+    {
+        cin >> n1 >> n2 >> n3;
+
+        Node* head1 = inputList(n1);
+        Node* head2 = inputList(n2);
+        Node* common = inputList(n3);
+
+        Node* temp = head1;
+        while (temp != NULL && temp->next != NULL)
+            temp = temp->next;
+        if (temp != NULL) temp->next = common;
+
+        temp = head2;
+        while (temp != NULL && temp->next != NULL)
+            temp = temp->next;
+        if (temp != NULL) temp->next = common;
+        
+        Solution ob;
+        cout << ob.intersectPoint(head1, head2) << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
+
+
+// Input: 
+// Linked List 1 = 4->1->common
+// Linked List 2 = 5->6->1->common
+// common = 8->4->5->NULL
+// Output: 8
